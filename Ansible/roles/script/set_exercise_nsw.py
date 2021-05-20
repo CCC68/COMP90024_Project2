4c48809f45eeec5eae1a54749d53b52a9c750a8c
Hanzhen Yang <hanzhen.yang@icloud.com>
2021-05-21 02:03:40 +0800

    setup all database and reduce function

--- Ansible/roles/script/set_exercise_nsw.py
+++ Ansible/roles/script/set_exercise_nsw.py
@@ -3,23 +3,23 @@ import json
 
 couch = couchdb.Server('http://adm1n_c0uchd6:_gr0up68H1_@localhost:5984')
 
-bitcoin_vic_db = couch.create('bitcoin_vic_db')
+exercise_nsw_db = couch.create('exercise_nsw_db')
 
-json_file = "/home/ubuntu/COMP90024_Project2/crawler/bitcoin_vic.json"
+json_file = "/home/ubuntu/COMP90024_Project2/crawler/exercise_nsw.json"
 
 with open(json_file) as f:
     line = f.readline()
     while line != "":
         try:
             db_entry = json.loads(line)
-            bitcoin_vic_db.save(db_entry)
+            exercise_nsw_db.save(db_entry)
         except Exception as e:
             print(e)
             line = f.readline()
         line = f.readline()
             
 docs = [{
-    "_id": "_design/bitcoin_location_count",
+    "_id": "_design/exercise_location_count",
     "language" : "javascript",
     "views": {
         "all": {
@@ -30,5 +30,5 @@ docs = [{
     }
 }]
 
-resultList = bitcoin_vic_db.update(docs)
-bitcoin_vic_db.resource.put("_security", {"members":{"roles":[]},"admins":{"roles":[]}})
+resultList = exercise_nsw_db.update(docs)
+exercise_nsw_db.resource.put("_security", {"members":{"roles":[]},"admins":{"roles":[]}})
