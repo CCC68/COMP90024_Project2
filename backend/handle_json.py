0c5527b230cb977e205d463a3000016181b8abf6
vinobryan <vino24bryan@gmail.com>
2021-05-21 11:40:22 +0800

    'vic_plus_all_aurin_data'

--- backend/handle_json.py
+++ backend/handle_json.py
@@ -5,10 +5,15 @@ import urllib.request
 
 Base_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
 
+# resceive the backend request of state
+state = 'vic'
 # aurin city income list
-df = pd.read_csv("aurin/aurin_vic/income.csv", encoding='utf-8', header=0, index_col=0)
+df_income = pd.read_csv(Base_dir+f"/aurin/aurin_{state}/income.csv", encoding='utf-8')
+df_obesity = pd.read_csv(Base_dir+f"/aurin/aurin_{state}/obesity.csv", encoding='utf-8')
+df_population = pd.read_csv(Base_dir+f"/aurin/aurin_{state}/population.csv", encoding='utf-8')
 # shift to lower case
-city_list = df.index.tolist()
+city_list = df_income['lga_name'].tolist()
+
 
 
 def get_record(url):
@@ -18,20 +23,18 @@ def get_record(url):
 
 def get_city_income(city):
 
-    median_aud_2013_14 = df.at[city, "median_aud_2013_14"]
-    median_aud_2014_15 = df.at[city, "median_aud_2014_15"]
-    return (int(median_aud_2013_14), int(median_aud_2014_15))
+    median_aud_2014_15 = df_income.loc[df_income['lga_name'] == city, "median_aud_2014_15"]
+    return int(median_aud_2014_15)
 
-# with open(Base_dir+"/aurin/aurin_vic/income.csv") as f:
-#     line = f.readline()
-#     while line != "":
-#         line = line.split(",")
-#         if line[0] == "Melbourne (C)":
-#             median_aud_2013_14 = line[1]
-#             median_aud_2014_15 = line[2]
-#             break;
-#         line = f.readline()
+def get_city_obesity(city):
 
+    median_aud_2014_15 = df_obesity.loc[df_obesity['lga_name'] == city, "ovrwgt_p_1_count"]
+    return int(median_aud_2014_15)
+
+def get_city_population(city):
+
+    median_aud_2014_15 = df_population.loc[df_population['lga_name'] == city, "erp_2016pr"]
+    return int(median_aud_2014_15)
 
 def get_total_count(city, map_json):
 
@@ -42,8 +45,8 @@ def get_total_count(city, map_json):
     return tweet_count
 
 
-with open('aurin/vic.json',encoding='utf-8',) as f2:
-    url = 'http://172.26.132.118:5984/bitcoin_vic_db/_design/bitcoint_location_count/_view/all?group=true'
+with open(Base_dir+f'/aurin/{state}.json',encoding='utf-8',) as f2:
+    url = f'http://172.26.132.118:5984/bitcoin_{state}_db/_design/bitcoint_location_count/_view/all?group=true'
 
     #raw_data 是原始的geo.json
     raw_data = json.load(f2)
@@ -56,11 +59,13 @@ with open('aurin/vic.json',encoding='utf-8',) as f2:
         for feature in raw_data['features']:
             if (feature['properties']["vic_lga__3"]).lower() == city:
                 feature['properties']['tweets_count'] = get_total_count(city, data1)
-                feature['properties']['income_2013'], feature['properties']['income_2014'] = get_city_income(city)
+                feature['properties']['income_2014'] = get_city_income(city)
+                feature['properties']['obesity_rate'] = get_city_obesity(city)
+                feature['properties']['population'] = get_city_population(city)
 
     #所有城市遍历完再写进去
-    with open('vic_plus.json', 'w') as r:
-        print (raw_data)
+    with open(Base_dir+f'/aurin/{state}_plus.json', 'w') as r:
+        print(raw_data)
         json.dump(raw_data, r)
 
     r.close()
